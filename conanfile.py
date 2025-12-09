f032f4e1c8bee822c0e5ec1bcbbb49a8fc389c50
Philippe Steinmann <philippe.steinmann@gmail.com>
2025-12-09 05:49:37 +0100

    Make the project work with Conan 2
    Also use Conan libusb
    GL-6

--- conanfile.py
+++ conanfile.py
@@ -15,12 +15,13 @@ class MdtTerminalConan(ConanFile):
   generators = "CMakeDeps", "VirtualBuildEnv"
 
   def requirements(self):
-    self.requires("qt/5.15.6")
-    self.requires("mdtitemmodel/0.0.2@scandyna/testing")
+    self.requires("qt/5.15.16")
+    self.requires("mdtitemmodel/0.0.3@scandyna/testing")
+    self.requires("libusb/1.0.29")
 
   def build_requirements(self):
-    self.test_requires("catch2/2.13.9")
-    self.test_requires("mdtcmakemodules/0.20.0@scandyna/testing")
+    self.test_requires("catch2/2.13.10")
+    self.test_requires("mdtcmakemodules/0.21.0@scandyna/testing")
 
   def generate(self):
     tc = CMakeToolchain(self)
