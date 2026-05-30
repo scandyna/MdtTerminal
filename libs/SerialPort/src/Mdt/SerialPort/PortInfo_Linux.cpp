a31627ad572971468d5db503af5ba5b976faefef
Philippe Steinmann <philippe.steinmann@gmail.com>
2026-05-30 12:30:41 +0200

    SerialPort: implement mdt_posix_open() to handle EINTR error and a memory lifetime bug

--- libs/SerialPort/src/Mdt/SerialPort/PortInfo_Linux.cpp
+++ libs/SerialPort/src/Mdt/SerialPort/PortInfo_Linux.cpp
@@ -13,16 +13,44 @@
 #include <QByteArray>
 #include <fcntl.h>        // open()
 #include <unistd.h>       // close()
+#include <errno.h>
 #include <sys/ioctl.h>
 #include <linux/serial.h> // serial_struct
 #include <cassert>
 
 namespace Mdt{ namespace SerialPort{
 
+/*! \internal Helper around POSIX open()
+ *
+ * This variant handles the EINTR error,
+ * and avoids a lifetime bug.
+ *
+ * See also Qt source code
+ * qtbase/src/corelib/kernel/qcore_unix_p.h
+ *
+ * \note No close() wrapper is provided today.
+ * It is NOT recommended to loop to filter out EINTR.
+ * It is safer to just call close() once.
+ *
+ * \todo Should go to some appropriate place
+ */
+int mdt_posix_open(const QString & path, int flags, mode_t mode = 0777)
+{
+  int fd;
+  const QByteArray baPath = path.toLocal8Bit();
+
+  do{
+    fd = ::open(baPath.constData(), flags, mode);
+  }while(fd == -1 && errno == EINTR);
+
+  return fd;
+}
+
 bool isExisting_ttyS_port(const QString & systemLocation)
 {
   const mode_t flags = O_RDWR | O_NONBLOCK | O_NOCTTY;
-  const int fd = ::open(systemLocation.toLocal8Bit().constData(), flags);
+  const int fd = mdt_posix_open(systemLocation, flags);
+  // const int fd = ::open(systemLocation.toLocal8Bit().constData(), flags);
   if(fd < 0){
     return false;
   }
